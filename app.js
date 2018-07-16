1345e3af669610f2a1a8ee27585aef9ea5247e72
Adriano J Soares <adrianojsoares1@gmail.com>
2018-07-16 00:58:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const CRoyaleAPI = require('./utils/client');
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTE2MCwiaWRlbiI6IjI2MjAxMzc3NDA3NTAwMjg4MSIsIm1kIjp7f' +
-  'SwidHMiOjE1MzEyODA2OTI4NDJ9.SqFdPptY7SmJNW6JgpdEHpgDvnjEtArsTnGknx_NLpw';
+const API_KEY = 'YOUR_KEY_HERE';
 
 const client = new CRoyaleAPI();
 
