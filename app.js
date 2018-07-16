5fc95f8eee2146344874fd6981ee5a7091b63de8
rail-trail <adrianojsoares1@gmail.com>
2018-07-16 00:55:46 -0400

    add documentation, error handling

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const CRoyaleAPI = require('./utils/client');
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTE2MCwiaWRlbiI6IjI2MjAxMzc3NDA3NTAwMjg4MSIsIm1kIjp7f' +
-    'SwidHMiOjE1MzEyODA2OTI4NDJ9.SqFdPptY7SmJNW6JgpdEHpgDvnjEtArsTnGknx_NLpw';
+  'SwidHMiOjE1MzEyODA2OTI4NDJ9.SqFdPptY7SmJNW6JgpdEHpgDvnjEtArsTnGknx_NLpw';
 
-const client = new CRoyaleAPI(API_KEY);
+const client = new CRoyaleAPI();
 
-client.player('2PCL28RV0', function(data){
-    console.log(`Player's name is: ${data.name}`);
+client.setApiKey(API_KEY);
+
+client.player('2PCL28RV0', function(err, data){
+  if(err)
+    throw(err);
+
+  console.log(`Player's name is: ${data.name} and he has ${data.trophies} trophies.`);
+
+  console.log(data);
 });
+
+/*
+client.clan('22QV8CC', ['name', 'tag'], function(err, data){
+  if(err)
+    throw(err);
+
+  console.log('Clan data is: ' + data.name + ' and the tag is: ' + data.tag);
+});
+
+*/
--- utils/client.js
+++ utils/client.js
@@ -1,59 +1,156 @@
 https = require('https');
 
 function Client(key) {
-    this.API_KEY = (typeof key === 'string') ? key : undefined;
-    this.REQUEST_OPTIONS = {
-        hostname: this.API_HOST,
-        port: 443,
-        method: 'GET',
-        headers: {
-            auth: this.API_KEY,
-            accept: 'application/json',
-            'Accept-Language': 'en-US',
-            connection: 'keep-alive'
-        }
-    };
+  this.API_KEY = (typeof key === 'string') ? key : undefined;
+
+
+  this.REQUEST_OPTIONS = {
+    hostname: this.API_HOST,
+    port: 443,
+    method: 'GET',
+    headers: {
+      auth: this.API_KEY,
+      accept: 'application/json',
+      'Accept-Language': 'en-US',
+      connection: 'keep-alive'
+    }
+  };
+
+  this.setApiKey = function (key) {
+    this.API_KEY = key;
+    this.REQUEST_OPTIONS['headers']['auth'] = this.API_KEY;
+  }
+
 }
 
+//API_HOST: The CR-API Host URI
 Client.prototype.API_HOST = 'api.royaleapi.com';
 
-Client.prototype.setApiKey = (key) => {
-    this.API_KEY = key;
-    return this;
-};
-
-Client.prototype._getGenericLog = function(path, callback) {
+//_getGenericLog: string, string, array, function -> void
+//purpose: Send a HTTP request on behalf of given path, and return that data.
+Client.prototype._getGenericLog = function(path, name, fields, callback) {
+    //ensure API key exists
     if(!this.API_KEY)
-        console.error('API Key has not been set, cannot proceed. Set it by calling setApiKey(key) on the client.');
+        throw new Error('API Key has not been set, cannot proceed. Set it by calling setApiKey(key) on the client.');
 
     this.REQUEST_OPTIONS.path = path;
     let partial_data = [];
 
+    //Make request - will use async streams
     const req = https.request(this.REQUEST_OPTIONS, function (res) {
+
+        //read partial data chunk from buffer
         res.on('data', (chunk) => partial_data.push(chunk));
-        res.on('end', () => callback(JSON.parse(partial_data.join(''))));
+
+        //Data transfer is complete, begin data extrapolation
+        res.on('end', function(){
+          let data = null, error = null;
+
+          switch(res.statusCode){
+
+            case 200: {
+              try {
+                data = getSpecificFields(fields, JSON.parse(partial_data.join('')));
+              } catch(e) {
+                error = new Error('The JSON was not able to be parsed. Check your request or try again later.');
+              }
+              break;
+            }
+
+            case 401: error = new Error('401 (Unauthorized). Most likely cause is an invalid API key.');
+              break;
+
+            case 404: error = new Error('404 (Not Found). This player or clan wasn\'t found. ' +
+              'Most likely the tag was typed incorrectly.');
+              break;
+
+            case 500: error = new Error('500 (Internal Server Error). Something is wrong with the RoyaleAPI servers, ' +
+              'this is out of my control!');
+              break;
+
+            default: error = new Error(`Unexpected Error: ${res.statusCode}: (${res.statusMessage}).`);
+          }
+          callback(error, data);
+        });
     });
 
-    req.on('error', (err) => console.error(err));
+    req.on('error', (err) => {
+      throw new Error(`The API returned ${err.statusCode}: ${err.statusMessage}.\nHear ye, Hear ye:\n${err}`);
+    });
+
     req.end();
 };
 
-Client.prototype.player = function(name, callback){
-    this._getGenericLog(`player/${name}`, callback);
+function getSpecificFields(fields, obj){
+  const new_obj = {};
+
+  for(let i = 0; i < fields.length; i++){
+    let field = obj[fields[i]];
+    if(!field)
+      console.warn(`Warning! Field ${field} does not exist! References will be 'undefined'.`);
+    else new_obj[fields[i]] = field;
+  }
+  return (fields.length === 0) ? obj : new_obj;
+}
+
+Client.prototype.player = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/player/${name}`, name, [], fields);
+  return this._getGenericLog(`/player/${name}`, name, fields, callback);
 };
 
-Client.prototype.arbnoPlayers = (names, callback) => this._getGenericLog(`player/${names.join(',')}`, callback);
+Client.prototype.arbnoPlayers = function(names, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/player/${names.join(',')}`, name, [], fields);
+  return this._getGenericLog(`/player/${names.join(',')}`, name, fields, callback);
+} ;
 
-Client.prototype.playerBattles = (name, callback) => this._getGenericLog(`player/${name}/battles`);
-Client.prototype.arbnoPlayerBattles = (names, callback) => this._getGenericLog(`player/${names.join(',')}/battles`);
+Client.prototype.playerBattles = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/player/${name}/battles`, name, [], fields);
+  return this._getGenericLog(`/player/${name}/battles`, name, fields, callback);
+};
 
-Client.prototype.playerChests = (name, callback) => this._getGenericLog(`player/${name}/chests`, callback);
-Client.prototype.arbnoPlayerChests = (names, callback) => this._getGenericLog(`player/${names.join(',')}`, callback);
+Client.prototype.arbnoPlayerBattles = function(names, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/player/${names.join(',')}/battles`, name, [], fields);
+  return this._getGenericLog(`/player/${names.join(',')}/battles`, name, fields, callback);
+};
 
-Client.prototype.clan = (name, callback) => this._getGenericLog(`/clan/${name}`, callback);
-Client.prototype.clanBattles = (name, callback) => this._getGenericLog(`/clan/${name}/battles`, callback);
+Client.prototype.playerChests = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/player/${name}/chests`, name, [], fields);
+  return this._getGenericLog(`/player/${name}/chests`, name, fields, callback);
+};
 
-Client.prototype.war = (name, callback) => this._getGenericLog(`/clan/${name}/war`, callback);
-Client.prototype.warLog = (name, callback) => this._getGenericLog(`/clan/${name}/warlog`, callback);
+Client.prototype.arbnoPlayerChests = function(names, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/player/${names.join(',')}/chests`, name, [], fields);
+  return this._getGenericLog(`/player/${names.join(',')}/chests`, name, fields, callback);
+};
+
+Client.prototype.clan = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/clan/${name}`, name, [], fields);
+  return this._getGenericLog(`/clan/${name}`, name, fields, callback);
+};
+
+Client.prototype.clanBattles = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/clan/${name}/battles`, name, [], fields);
+  return this._getGenericLog(`/clan/${name}/battles`, name, fields, callback);
+};
+
+Client.prototype.war = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/clan/${name}/war`, name, [], fields);
+  return this._getGenericLog(`/clan/${name}/war`, name, fields, callback);
+};
+
+Client.prototype.warLog = function(name, fields, callback){
+  if(typeof fields === 'function')
+    return this._getGenericLog(`/clan/${name}/warlog`, name, [], fields);
+  return this._getGenericLog(`/clan/${name}/warlog`, name, fields, callback);
+};
 
 module.exports = Client;
